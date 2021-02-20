3bbf1b229f68a6edd8fe8fdc60950f357cb90cba
Montague Monro <montymonro1@gmail.com>
2021-02-20 16:29:14 +0000

    add comments and line break support

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import uos
 import machine
 import utime
 
+# Credit to https://helloraspberrypi.blogspot.com/2021/02/raspberry-pi-picomicropython-pair-hc-05.html for HC-06/05 guide
+
 # Raspberry Pi Pico/MicroPython
 # Pair HC-06 connected to UART0
 
@@ -9,20 +11,15 @@ import utime
 # UART(0, baudrate=9600, bits=8, parity=None, stop=1, tx=0, rx=1)
 
 # Connection:
-# RPi Pico UART0  HC-06
-# GP0(pin 1)      RX
-# GP1(pin 2)      TX
+# RPi Pico UART0  | HC-06
+# -----------------------
+# GP0(pin 1)      | RX
+# GP1(pin 2)      | TX
 
 
-# To enter AT-Command mode-
-
-# HC06:
-# Power-up in NOT CONNECTED
-
-print(uos.uname())
-uart0 = machine.UART(0, baudrate=9600)  # at-command
-
-# 2 sec timeout is arbitrarily chosen
+print(uos.uname())  # Print pico system info
+# Initialise new UART instance with a baudrate of 9600 to match the HC-06 default
+uart0 = machine.UART(0, baudrate=9600)
 
 
 def sendCMD_waitResp(cmd, uart=uart0, timeout=2000):
@@ -37,11 +34,13 @@ def waitResp(uart=uart0, timeout=2000):
     resp = b""
     while (utime.ticks_ms()-prvMills) < timeout:
         if uart.any():
-            resp = b"".join([resp, uart.read(1)])
-    return str(resp, 'utf-8')
+            resp = uart.readline()
+    return str(resp.rstrip(), 'utf8')
 
 
+# Onboard LED, currently used for feedback that the program has begun running
 led_onboard = machine.Pin(25, machine.Pin.OUT)
+# LED to toggle when receiving the correct command via bluetooth
 led_green = machine.Pin(15, machine.Pin.OUT)
 
 # indicate program started visually
@@ -51,19 +50,20 @@ led_onboard.value(1)
 utime.sleep(1.0)
 led_onboard.value(0)
 
-print(uart0)
+print(uart0)  # Print UART details for the bluetooth connection
 print("Starting Bluetooth...")
 
 while True:
-    response = waitResp()
+    response = waitResp()  # Await command from bluetooth with 2s timeout
 
     if response:
         print(response)
-        if response == '1':
+        if response == '1':  # If char 1 is sent, turn on LED
             print("Setting LED on")
             led_green.high()
-        elif response == '0':
+        elif response == '0':  # If char 0 is sent, turn off LED
             print("Setting LED off")
             led_green.low()
         else:
+            # Handle unrecognised inputs other than 1 and 0
             print("Error: unrecognised input")
